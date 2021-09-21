e5307b7914a8989c3fee547fb504dc466c1dc120
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-20 22:54:13 -0300

    devolviendo arreglo de usuarios con otro get

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World by Express');
 });
 
+app.get('/api/usuarios', (req, res) => {
+    res.send(['Carmelo', 'Lucas', 'Paula']);
+});
+
 app.listen(3000, () => {
     console.log('Servidor funcionando en el puerto 3000.')
 });
